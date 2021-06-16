73f5887720d75b50780a5b08111886b826452b86
davidkrck <62652531+davidkrck@users.noreply.github.com>
2021-06-16 22:44:31 +0200

    Add files via upload

--- http_get.py
+++ http_get.py
@@ -4,37 +4,45 @@ import sys
 import re
 import ssl
 
+URL = sys.argv[1]
+
 def url(URL):
 	temp = ""
 	path = ""
 	hostname = ""
-	ssl = ""
-	temp = re.match("(\w+)://", URL)
+	ssl_protocol = ""
+	zvysok = ""
+	temp = re.match("^htt(p|ps)", URL)
 	if temp:
-		ssl = temp[0]
-	temp = re.match("://([\w\-\.]+)", URL)
-	if temp:
-		hostname = temp[0]
-	temp = re.match("\w(/.*|$)", URL)
-	if temp:
-		path = temp[0]
-	return (ssl, hostname, path)
+		temp = re.split("://",URL)
+		ssl_protocol = temp[0]
+		zvysok = temp[1]
 
-URL = sys.argv[1]
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	temp = zvysok.split("/", maxsplit = 1)
+	hostname = temp[0]
+	path = temp[1]
+
+	return (ssl_protocol, hostname, path)
 
 while True:
-	ssl, hostname, path = url(URL)
+	ssl_protocol, hostname, path = url(URL)
+	#print(ssl_protocol+"\n"+hostname+"\n"+path)
 	
-	if ssl == "https":
+	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	
+	if (ssl_protocol == "") or (hostname == ""):
+		sys.stderr.write("error")
+		sys.exit(2)
+
+	if ssl_protocol == "https":
 		s.connect((hostname,443))
 		s=ssl.wrap_socket(s)
 	else:
 		s.connect((hostname,80))
 
 	f=s.makefile("rwb")
-	f.write(f"GET {path} HTTP/1.1\r\n".encode("ASCII"))
-	f.write(f"Host: {hostname} \r\n".encode("ASCII"))
+	f.write(f"GET /{path} HTTP/1.1\r\n".encode("ASCII"))
+	f.write(f"Host:{hostname}\r\n".encode("ASCII"))
 	f.write(("Accept-charset: UTF-8\r\n\r\n").encode("ASCII"))
 	f.flush()
 
@@ -48,21 +56,33 @@ while True:
 	headre = {}
 	
 	while True:
-		header = f.readline.decode("ASCII")
-		dict = header.split(": ")
-		headre[dict[0].lower()] = dict[1].lower()
-
+		header = f.readline().decode("ASCII")
+		if not header:
+			break
+		if not ":" in header:
+			break
+		if header == "\r\n":
+			break
+		h = header.split(":",1)
+		headre[h[0].lower()] = h[1].lower()
+	
 	if status == "200":
 		break
-
-	elif status == ("301" or "302" or "303" or "307" or "308"):
-		(ssl, hostname, path) = url(headre["location"])
-		f.close()
+	elif ("301") or ("302") or ("303") or ("307") or ("308") == status:
+		if ("location" in headre.keys()):
+			URL = headre["location"]
+			f.close()
+			s.close()
+			sys.exit(1)
+		else:
+			f.close()
+			s.close()
+			sys.exit(1)
 	else:
-		sys.stderr.write(status + popis)
+		sys.stderr.write(status + " " + popis + "\n")
 		f.close()
+		s.close()
 		sys.exit(1)
-		break
 
 for i in headre.keys():
 	if i == "content-length":
@@ -70,19 +90,17 @@ for i in headre.keys():
 		obsah = f.read(length)
 		sys.stdout.buffer.write(obsah)
 		break
-
+		
 	elif i == "transfer-encoding":
 		while True:
-			length = f.readline().decode("ASCII")
-			length = int(length, 16)
+			length = f.readline().decode("ASCII").strip()
+			length = int(length, base = 16)
 			chunk = f.read(length)
 			sys.stdout.buffer.write(chunk)
 			if not chunk:
 				break
 			f.readline()
-		break
 
-f.flush()
 f.close()
 s.close()
-sys.exit(0)
+sys.exit(1)
